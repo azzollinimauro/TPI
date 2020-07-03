0b65a98c7743b54f00607be32e861368a1c8e961
abrezigar <abrezigar@gmail.com>
2020-07-03 19:08:47 -0300

    Se agregan los test para acelerar y ralentizar

--- tests/acelerar.cpp
+++ tests/acelerar.cpp
@@ -22,3 +22,50 @@ TEST(acelerarTEST, aceleracionValida){
 }
 
 
+TEST(acelerarTEST, diferentesSeniales){
+    senial hablante1 = {1, 3, -3, 4, 6, 0, -2, -8, 9, -15, 7, 1, 3, -3, 4, 6, 0, -2, -8, 9, -15};
+    senial hablante2 = {6, 7, -15, 9, 2, 3, -7, 8, 4, 0, 3, -2, 4, -3, 5, 1, 3, 0, -2, 6, 0};
+    reunion reunionOriginal = {make_pair(hablante1, 0), make_pair(hablante2, 1)};
+    int prof = 8;
+    int freq = 10;
+
+    senial hablante1Acelerado = {3, 4, 0, -8, -15, 1, -3, 6, -2, 9};
+    senial hablante2Acelerado = {7, 9, 3, 8, 0, -2, -3, 1, 0, 6};
+    reunion reunionAcelerada = {make_pair(hablante1Acelerado, 0), make_pair(hablante2Acelerado, 1)};
+
+    acelerar(reunionOriginal, prof, freq);
+
+    ASSERT_REUNION_EQ(reunionOriginal, reunionAcelerada);
+}
+
+TEST(acelerarTEST, senialesPares){
+    senial hablante1 = {1, 3, -3, 4, 6, 0, -2, -8, 9, -15, 7, 1, 3, -3, 4, 6, 0, -2, -8, 9, -15, 6};
+    senial hablante2 = {6, 7, -15, 9, 2, 3, -7, 8, 4, 0, 3, -2, 4, -3, 5, 1, 3, 0, -2, 6, 0, 1};
+    reunion reunionOriginal = {make_pair(hablante1, 0), make_pair(hablante2, 1)};
+    int prof = 8;
+    int freq = 10;
+
+    senial hablante1Acelerado = {3, 4, 0, -8, -15, 1, -3, 6, -2, 9, 6};
+    senial hablante2Acelerado = {7, 9, 3, 8, 0, -2, -3, 1, 0, 6, 1};
+    reunion reunionAcelerada = {make_pair(hablante1Acelerado, 0), make_pair(hablante2Acelerado, 1)};
+
+    acelerar(reunionOriginal, prof, freq);
+
+    ASSERT_REUNION_EQ(reunionOriginal, reunionAcelerada);
+}
+
+TEST(acelerarTEST, resultanteIgualAFreq){
+    senial hablante1 = {1, 3, -3, 4, 6, 0, -2, -8, 9, -15, 7, 1, 3, -3, 4, 6, 0, -2, -8, 9};
+    senial hablante2 = {6, 7, -15, 9, 2, 3, -7, 8, 4, 0, 3, -2, 4, -3, 5, 1, 3, 0, -2, 6};
+    reunion reunionOriginal = {make_pair(hablante1, 0), make_pair(hablante2, 1)};
+    int prof = 8;
+    int freq = 10;
+
+    senial hablante1Acelerado = {3, 4, 0, -8, -15, 1, -3, 6, -2, 9};
+    senial hablante2Acelerado = {7, 9, 3, 8, 0, -2, -3, 1, 0, 6};
+    reunion reunionAcelerada = {make_pair(hablante1Acelerado, 0), make_pair(hablante2Acelerado, 1)};
+
+    acelerar(reunionOriginal, prof, freq);
+
+    ASSERT_REUNION_EQ(reunionOriginal, reunionAcelerada);
+}
--- tests/ralentizar.cpp
+++ tests/ralentizar.cpp
@@ -21,4 +21,37 @@ TEST(ralentizarTEST, ralentizacionValida){
     ASSERT_REUNION_EQ(reunionOriginal, reunionRalentizada);
 }
 
+TEST(ralentizarTEST, ralentizarDiferentesSeniales){
+    senial hablante1 = {1, 3, -3, 4, 6, 0, -2, -8, 9, -15};
+    senial hablante2 = {4, -2, 1, 0, 3, 4, 4, 2, 1, -13};
+    reunion reunionOriginal = {make_pair(hablante1, 0), make_pair(hablante2, 1)};
+    int prof = 8;
+    int freq = 10;
 
+    senial hablante1Acelerado = {1, 2, 3, 0, -3, 0, 4, 5, 6, 3, 0, -1, -2, -5, -8, 0, 9, -3, -15};
+    senial hablante2Acelerado = {4, 1, -2, 0, 1, 0, 0, 1, 3, 3, 4, 4, 4, 3, 2, 1, 1, -6, -13};
+    reunion reunionRalentizada = {make_pair(hablante1Acelerado, 0), make_pair(hablante2Acelerado, 1)};
+
+    ralentizar(reunionOriginal, prof, freq);
+
+    ASSERT_REUNION_EQ(reunionOriginal, reunionRalentizada);
+}
+
+TEST(ralentizarTEST, ralentizarSenial0){
+    senial hablante1 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    senial hablante2 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    senial hablante3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    reunion reunionOriginal = {make_pair(hablante1, 0), make_pair(hablante2, 1), make_pair(hablante3, 2)};
+    int prof = 8;
+    int freq = 10;
+
+    senial hablante1Acelerado = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    senial hablante2Acelerado = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    senial hablante3Acelerado = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    reunion reunionRalentizada = {make_pair(hablante1Acelerado, 0), make_pair(hablante2Acelerado, 1),
+                                    make_pair(hablante3Acelerado, 2)};
+
+    ralentizar(reunionOriginal, prof, freq);
+
+    ASSERT_REUNION_EQ(reunionOriginal, reunionRalentizada);
+}
